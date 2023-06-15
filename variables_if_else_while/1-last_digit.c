09127aa007b8fd2f3ba75868cbcd39b9662c82ea
seba17962 <isebsebastiansilva@gmail.com>
2023-06-15 08:46:19 -0700

    printf two variables

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int
+main(void)
+{
+	int n, number, last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	last_digit = number % 10;
+	if (n > 5)
+	{
+		printf("Last digit of %d is %i and is greater than 5", n, last_digit);
+	}
+	if (n == 5)
+	{
+		printf("Last digit of %d is %i and is 0", n, last_digit);
+	}
+	if (n < 6 && n != 0)
+	{
+		printf("Last digit of %d is %i and is less than 6 and not 0", n, last_digit);
+	}
+	return (0);
+}
