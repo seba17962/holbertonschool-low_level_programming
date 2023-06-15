40748bbe5792cfc153de2f1ec4e01b6a11b626b6
seba17962 <isebsebastiansilva@gmail.com>
2023-06-15 10:35:05 -0700

    alphabet lower and upper

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int
+main(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+	putchar(alphabet);
+	if (alphabet == 'q' || alphaber == 'e')
+		continue
+}
+putchar('\n');
+return (0);
+}
