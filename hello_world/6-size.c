993ad77bbbaeab6bd1c0215088e150415c561c54
seba17962 <isebsebastiansilva@gmail.com>
2023-06-14 18:31:02 -0700

    add printf the size of various Types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	char charType;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-	printf("Size of an int: %zu bytes\n", sizeof(intType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
 	printf("Size of a long long: %zu byte(s)\n", sizeof(longlongType));
 	printf("Size of float %zu byte(s)\n", sizeof(floatType));
