503f01175cb0bce36f089b9165bc2c226c3974f3
seba17962 <isebsebastiansilva@gmail.com>
2023-06-17 15:38:18 -0700

    Times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,38 +8,26 @@
 */
 void times_table(void)
 {
-	int n;
+	int n, mult, prod;
 
 	for (n = 0; n <= 9; n++)
 	{
-		_putchar(n * 0);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n * 1);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n * 2);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n * 3);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n * 4);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n * 5);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n * 6);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n * 7);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n * 8);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n * 9);
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = n * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+		}
 		_putchar('\n');
 	}
 }
