0f2c85aaf11affa2a7253ea9eca0ab5dc69005d3
seba17962 <isebsebastiansilva@gmail.com>
2023-06-17 07:26:43 -0700

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+	int c;
+
+	if (_islower(c))
+	{
+		_putchar('1');
+	}
+	else
+		_putchar('0');
+
+}
