4adcf2c04bbc2f08fbf03dd1d03e3088910f8c81
seba17962 <isebsebastiansilva@gmail.com>
2023-06-20 13:44:43 -0700

    its ok?

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_diagonal - print 'n' times "\"
+ *@n: the number of times the character should be repeated
+ */
+
+void print_diagonal(int n)
+{
+	int c = 0;
+
+	if (n != 0)
+	{
+	while (c < n)
+	{
+		_putchar(92);
+		c++;
+	}
+	}
+	_putchar('\n');
+}
