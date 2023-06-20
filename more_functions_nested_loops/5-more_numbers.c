af110719107515937316b375cf4c4a971112d74a
seba17962 <isebsebastiansilva@gmail.com>
2023-06-20 10:01:05 -0700

    its oke?

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * more_numbers - print 0 - 14
+ *
+ */
+
+void more_numbers(void)
+{
+	int n;
+
+		for (n = 0; n <= 14; n++)
+		{
+			if (n <= '9')
+			{
+				_putchar(n);
+			}
+			else
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+		}
+	_putchar('\n');
+}
