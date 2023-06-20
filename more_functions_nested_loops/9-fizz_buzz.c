803e97882f407f1e273e6f63e06bcf394ff7b1c7
seba17962 <isebsebastiansilva@gmail.com>
2023-06-20 16:13:58 -0700

    its ok?

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,19 +12,26 @@ int main(void)
 {
 	int n;
 
-	for (n = 1; n < 100; n++)
+	for (n = 1; n <= 100; n++)
 	{
 		if (n % 3 == 0 && n % 5 != 0)
 			printf("Fizz ");
+
 		else if (n % 5 == 0 && n % 3 != 0)
 			printf("Buzz ");
+
 		else if (n % 3 == 0 && n % 5 == 0)
 			printf("FizzBuzz ");
+
 		else
 			printf("%d ", n);
+
+		if (n == 100)
+			continue;
+		printf(" ");
 	}
-	if (n == 100)
-		printf("Buff");
+
 	putchar ('\n');
-return (0);
+
+	return (0);
 }
