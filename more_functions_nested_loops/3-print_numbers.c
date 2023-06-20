61690aa0cabb9d895a62acdbb99117dfb4a5fa81
seba17962 <isebsebastiansilva@gmail.com>
2023-06-20 07:10:54 -0700

    its ok?

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -4,14 +4,16 @@
 
 /**
  * print_numbers - print numbers from 0 to 9 followed by a new line
- * @n: de number to print
  *
  */
 
 void print_numbers(void)
 {
 	int n;
+
 	for (n = 0; n <= 9; n++)
+	{
 		_putchar(n);
 		_putchar('\n');
+	}
 }
