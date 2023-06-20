6ffe7afbaea459fa2824f87f05953cfd250f616d
seba17962 <isebsebastiansilva@gmail.com>
2023-06-20 13:34:27 -0700

    its ok?

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_line - print 'n' times "_"
+ *@n: the number of times the character should be repeated
+ */
+
+void print_line(int n)
+{
+	int c;
+
+	if (n != 0)
+	{
+	while (c <= n)
+	{
+		_putchar('_');
+		c++;
+	}
+	}
+	_putchar(92);
+	_putchar('n');
+}
