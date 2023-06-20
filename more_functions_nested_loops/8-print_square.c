ce891c70f226ca13dafbf61542bc4c62bb7a24ad
seba17962 <isebsebastiansilva@gmail.com>
2023-06-20 15:27:25 -0700

    its ok?

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -11,7 +11,7 @@ void print_square(int size)
 {
 	int length, width;
 
-	if (size != 0)
+	if (size > 0)
 	{
 		for (length = 0; length < size; length++)
 		{
@@ -21,5 +21,18 @@ void print_square(int size)
 			}
 			_putchar('\n');
 		}
+		_putchar('\n');
+	}
+	else if (size < 0)
+	{
+		for (length = 0; length > size; length--)
+		{
+			for (width = 0; width > size; width--)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+		_putchar('\n');
 	}
 }
