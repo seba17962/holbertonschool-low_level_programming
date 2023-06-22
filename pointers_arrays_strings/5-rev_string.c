3032a31a0a697fc4c6661bcbbd032d8ddbd01eed
seba17962 <isebsebastiansilva@gmail.com>
2023-06-22 13:53:41 -0700

    no more pleas

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -13,13 +13,15 @@ void print_rev(char *s)
 	int c = 0;
 	char *srev;
 
+	srev = s;
+
 	while (s[c] != '\0')
 		c++;
 
-	while (c > 0)
+	while (c >= 0)
 	{
 		c--;
-		s[c] = srev;
+		srev[c] = s[c];
 	}
 	_putchar('\n');
 }
