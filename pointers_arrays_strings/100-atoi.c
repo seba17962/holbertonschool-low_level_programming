17d7c610938e0471af54c7dd3524928e157f6441
seba17962 <isebsebastiansilva@gmail.com>
2023-06-24 09:46:48 -0700

    variable inicial in 0

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,7 +12,7 @@
 int _atoi(char *s)
 {
 	int sig = 1;
-	unsigned int res;
+	int res = 0;
 	int nb;
 
 	do {
@@ -26,7 +26,7 @@ int _atoi(char *s)
 
 		else if (res == 0)
 		{
-			break;
+			return (0);
 		}
 		} while (*s++);
 	nb = res * sig;
