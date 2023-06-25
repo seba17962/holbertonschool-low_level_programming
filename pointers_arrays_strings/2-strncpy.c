06ca19502ba65bfbbfb5cf1300a4bb7303cf8c3f
seba17962 <isebsebastiansilva@gmail.com>
2023-06-25 13:02:27 -0700

    first try

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copy n bytes of a second strings.
+ *@src: the string to copy.
+ *@dest: the destination of copy.
+ *@n: the number of bytes to copy of src.
+ *Return: a pointer to the resulting string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int len_src = 0;
+
+	dest = "";
+	while (src[len_src] != '\0')
+	{
+	len_src++;
+	}
+
+	for (i = 0; src[i] != src[n]; i++)
+	{
+	dest[i] = src[i];
+	}
+	return (dest);
+}
