d2a9c99396daa2d00aff58d088c837c04d5ab530
seba17962 <isebsebastiansilva@gmail.com>
2023-06-25 11:35:29 -0700

    first try

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings.
+ *@src: the string to add.
+ *@dest: the destination string.
+ *Return: a pointer to the resulting string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len_src = 0;
+	int len_dest = 0;
+
+	while (src[len_src] != '\0')
+	{
+	len_src++;
+	}
+	while (dest[len_dest] != '\0')
+	{
+	len_dest++;
+	}
+
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	dest[len_dest + i] = src[i];
+	}
+
+	dest[len_dest + i] = '\0';
+
+	return (dest);
+}
