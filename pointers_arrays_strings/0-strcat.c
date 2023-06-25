1e4c86067e7d55ffe9cbe7cd12c8ba36f6e40f8c
seba17962 <isebsebastiansilva@gmail.com>
2023-06-25 11:50:12 -0700

    fix the error of the declaration of 'i'

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,6 +9,7 @@
 
 char *_strcat(char *dest, char *src)
 {
+	int i;
 	int len_src = 0;
 	int len_dest = 0;
 
@@ -20,8 +21,6 @@ char *_strcat(char *dest, char *src)
 	{
 	len_dest++;
 	}
-
-	int i;
 
 	for (i = 0; src[i] != '\0'; i++)
 	{
