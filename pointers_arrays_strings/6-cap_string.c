eeac760bc5832bbe64a1c754cd09b7ca702245a5
seba17962 <isebsebastiansilva@gmail.com>
2023-06-26 15:31:34 -0700

    fix the first leter in uppercase

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -21,7 +21,7 @@ char *cap_string(char *s)
 	{
 		for (j = 0; j < 14; j++)
 		{
-			if (s[i] == sep[j] && s[i + 1] > 96)
+			if (s[i] == sep[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
 			{
 				s[i + 1] = s[i + 1] - 32;
 			}
