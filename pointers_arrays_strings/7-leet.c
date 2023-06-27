f5499046f35d360b4753999d2a4e099b4ef1176c
seba17962 <isebsebastiansilva@gmail.com>
2023-06-27 06:23:18 -0700

    woow

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *leet - this function wncode a string into 1337
+ *
+ *@s: the string to encode into 1337
+ * Return: 
+ */
+
+
+char *leet(char *s)
+{
+	int i, j;
+	char code[6] = {'a', 'e', 'o', 't', 'l'};
+	char encod[6] = {'4', '3', '0', '7', '1'}
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == code[j] || s[i] == code[j] - 32)
+			{
+				s[i] = encode[j]
+			}
+		}
+	}
+	return (s)
+}
