09ab41cf0c1ec127911ba8cb5f284145c2b8aee2
seba17962 <isebsebastiansilva@gmail.com>
2023-06-27 15:44:58 -0700

    fix second while, s[j]<=i

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,17 +10,22 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0;
-	unsigned int j = 0;
+	int i;
+	int j;
+	unsigned int num = 0;
 
-	while (accept[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		i++;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				num++;
+				break;
+			}
+			else if (accept[j + 1] == '\0')
+				return (num);
+		}
 	}
-
-	while (s[j] <= i)
-	{
-		j++;
-	}
-	return (j);
+	return (num);
 }
