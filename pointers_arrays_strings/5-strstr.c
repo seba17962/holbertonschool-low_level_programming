894013a481cb52eba17ebcbd85943faa9ec6ba5f
seba17962 <isebsebastiansilva@gmail.com>
2023-06-27 16:44:00 -0700

    i need a do while

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -9,11 +9,20 @@
 
 char *_strstr(char *haystack, char *needle)
 {
+	int i = 0;
+
+	if (*needle == 0)
+		return (haystack);
+
 	while (*haystack)
 	{
 		if (*haystack == *needle)
 		{
-			return (haystack);
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			} while (haystack[i] == needle[i]);
 		}
 		haystack++;
 	}
