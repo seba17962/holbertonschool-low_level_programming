08cb80b73897a2ce34be2982bdb9280e0c25d43a
seba17962 <isebsebastiansilva@gmail.com>
2023-06-28 10:30:01 -0700

    en tu boca

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -6,16 +6,15 @@
  * Return: the length
  */
 
+
 int _strlen_recursion(char *s)
 {
 	int len = 0;
 
-	if (*s == '\0')
+	if (*s)
 	{
-		return;
-	}
 		len++;
-		_strlen_recursion(s + 1);
-	
+		len += _strlen_recursion(s + 1);
+	}
 	return (len);
 }
