a0a9817d3eba73f3f819e42b25f61c9dd7530552
seba17962 <isebsebastiansilva@gmail.com>
2023-06-28 11:21:36 -0700

    to try

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - raised x to the power of y
+ * @x: the base.
+ * @y: the power
+ * Return: raised x to the power of y, if y slower than 0, return -1
+ */
+
+
+int _pow_recursion(int x, int y)
+{
+
+	if (y < 0)
+		return (-1);
+
+	if (y != 0)
+	{
+		x *= _pow_recursion(x, y--);
+	}
+	return (x);
+}
