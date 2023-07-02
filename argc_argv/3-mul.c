cebf7bb22e79a6a6a0bf2c4aa19a064f338d2ee2
seba17962 <isebsebastiansilva@gmail.com>
2023-07-02 10:22:53 -0700

    atoi

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+		return (0);
+	}
+	else if (argc <= 1)
+	{
+		printf("Error");
+		return (1);
+	}
+}
