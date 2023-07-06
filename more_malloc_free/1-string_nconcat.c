b398d14cd32571d64dc8cdffe272912253a269c9
seba17962 <isebsebastiansilva@gmail.com>
2023-07-06 09:18:56 -0700

    funcion to cocatenate two strings

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	while (s1[len1] != '\0')
 		len1++;
-	while (len2 <= n && s2[len2] != '\0')
+	while (len2 < n && s2[len2] != '\0')
 		len2++;
 
 	mem = malloc(sizeof(char) * (len1 + len2 + 1));
