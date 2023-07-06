a95d57862494b2679a4f3218bcd91697ecdf0eb9
seba17962 <isebsebastiansilva@gmail.com>
2023-07-05 19:42:18 -0700

    not return

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *	**alloc_grid - return a pointer to a dimensional array of integres
+ *	@width:the width of the bidimensiona pointer
+ *	@height: the height of the bidimensional pointer
+ *	Return: a bidimentional pointer
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **p = NULL;
+	int i = 0, j = 0;
+
+	if (width == 0 || height == 0 || width < 0 || height < 0)
+		return (NULL);
+
+	p = malloc(sizeof(int *) * height);
+
+	if (*p == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		p[i] = malloc(sizeof(int) * width);
+
+		if (p == NULL)
+			return (NULL);
+
+		for (j = 0; j < width; j++)
+		{
+			p[i][j] = 0;
+		}
+	}
+	return (p);
+}
