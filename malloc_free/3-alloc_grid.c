8be49430434ed60eb65db3256c3f1a120511c921
seba17962 <isebsebastiansilva@gmail.com>
2023-07-05 20:01:20 -0700

    fix the segmentation fault

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -11,23 +11,27 @@
 int **alloc_grid(int width, int height)
 {
 	int **p = NULL;
-	int i = 0, j = 0;
+	int i = 0, j = 0, k = 0;
 
 	if (width == 0 || height == 0 || width < 0 || height < 0)
 		return (NULL);
 
 	p = malloc(sizeof(int *) * height);
 
-	if (*p == NULL)
+	if (p == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
 		p[i] = malloc(sizeof(int) * width);
 
-		if (p == NULL)
+		if (p[i] == NULL)
+		{
+			for (k = 0; k < i; k++)
+				free(p[k]);
+			free(p);
 			return (NULL);
-
+		}
 		for (j = 0; j < width; j++)
 		{
 			p[i][j] = 0;
