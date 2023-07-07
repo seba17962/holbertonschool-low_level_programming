0f2f4155522caa79611f95d20632ac6b34771aea
seba17962 <isebsebastiansilva@gmail.com>
2023-07-07 11:46:41 -0700

    fix the betty style

--- preprocessor/0-object_like_macro.h
+++ preprocessor/0-object_like_macro.h
@@ -1,7 +1,6 @@
-#ifndef 0-object_like_macro.h
-#define 0-object_like_macro.h
-
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
 
-#endif /* 0-object_like_macro.h */
+#endif /* OBJECT_LIKE_MACRO_H */
