6e1d00b08d2dfd491e1e2f9125d8142d2eb2f3e5
seba17962 <isebsebastiansilva@gmail.com>
2023-07-10 09:01:58 -0700

    delete the malloc, and fix if *d != null

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -12,12 +12,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
-	if (d == NULL)
-		return;
-
+	if (d != NULL)
+	{
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-
+	}
 }
