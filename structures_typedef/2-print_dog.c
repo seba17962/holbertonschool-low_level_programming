7423f37a43f68dde2ba87bd87c2fe588a76e3818
seba17962 <isebsebastiansilva@gmail.com>
2023-07-10 10:14:52 -0700

    fix if d == NULL

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,14 +9,14 @@
 
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+		return;
+
 	if (d->name == NULL)
 		d->name = "(nil)";
 
 	if (d->owner == NULL)
 		d->owner = "(nil)";
-
-	if (d == NULL)
-		return;
 
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
