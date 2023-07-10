c11d010807b5fb17fac8c8cefbe6c3c3006a5a2d
seba17962 <isebsebastiansilva@gmail.com>
2023-07-10 07:16:54 -0700

    add README and structure of dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * struct dog - short description
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+
+
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
