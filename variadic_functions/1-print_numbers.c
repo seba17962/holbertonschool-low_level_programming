8f960e31318cd9be2e308d800a1314ab76aabf85
seba17962 <isebsebastiansilva@gmail.com>
2023-07-12 16:28:28 -0700

    fix the space after the operator

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		if (i < n - 1)
 		{
-			printf("%d%s ", va_arg(num, int), separator);
+			printf("%d%s", va_arg(num, int), separator);
 		}
 		else
 			printf("%d\n", va_arg(num, int));
