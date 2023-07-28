f041a91b155df89698fad4a6683ae25f17f2b5c5
seba17962 <isebsebastiansilva@gmail.com>
2023-07-28 11:59:57 -0700

    fix the code to call the adder files

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,34 +14,30 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *aux = NULL;
 	unsigned int i = 0;
 
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-
 	aux = (*h);
-	new_Node = (dlistint_t *)malloc(sizeof(dlistint_t));
-	if (new_Node == NULL)
-		return (NULL);
-	new_Node->n = n;
-	new_Node->next = NULL;
-	new_Node->prev = NULL;
-	while (aux != NULL)
+	while (aux->next != NULL)
 	{
-		if (i == (idx - 1))
-		{
-			new_Node->prev = aux->prev;
-			new_Node->next = aux;
-			aux->prev->next = new_Node;
-			aux->prev = new_Node;
-			return (new_Node);
-		}
 		aux = aux->next;
 		i++;
 	}
-	if (i == idx)
-	{
-		aux->next = new_Node;
-		new_Node->prev = aux;
-		return (new_Node);
-	}
-	return (NULL);
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	if (i == idx && aux->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new_Node = (dlistint_t *)malloc(sizeof(dlistint_t));
+	if (new_Node == NULL)
+		return (NULL);
+
+	new_Node->n = n;
+	new_Node->next = NULL;
+	new_Node->prev = NULL;
+
+	new_Node->prev = aux->prev;
+	new_Node->next = aux;
+	aux->prev->next = new_Node;
+	aux->prev = new_Node;
+	return (new_Node);
 }
