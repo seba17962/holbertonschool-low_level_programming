1703e3f48a063d1e907494318a27fce277680aaf
seba17962 <isebsebastiansilva@gmail.com>
2023-07-28 06:51:29 -0700

    changed pointer asignation

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -23,8 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new_Node->next = *h;
-		if (*h == NULL)
-			*h = new_Node;
+		new_Node->next->prev = new_Node;
 		*h = new_Node;
 		return (new_Node);
 	}
