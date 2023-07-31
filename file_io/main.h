f25b6f65d5eadf607ea4dbfcceae85e0836d8c7e
seba17962 <isebsebastiansilva@gmail.com>
2023-07-31 08:31:12 -0700

    add lib

--- file_io/main.h
+++ file_io/main.h
@@ -5,6 +5,8 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
