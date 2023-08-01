a48fdeae661d7b8701a9118f38866b2f22f3d61d
seba17962 <isebsebastiansilva@gmail.com>
2023-08-01 08:04:06 -0700

    fix the return when the text_content its NULL

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
-		return (-1);
+		return (1);
 
 	fd = open(filename, O_RDWR | O_APPEND);
 	if (fd == -1)
